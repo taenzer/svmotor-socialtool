c759233048553477673fd03e7168b44f0351010b
Moritz Tanz <mtanz@stud.htwk-leipzig.de>
2023-05-13 14:16:47 +0200

    * Database Repair

--- app/inc/class-database.php
+++ app/inc/class-database.php
@@ -21,4 +21,38 @@ class DB{
       unset($properties['c']);
       return $properties;
     }
+
+    public function getMysqli(){
+      return $this->mysqli;
+    }
+
+    protected function getDbDebugLink(){
+      $id = uniqid();
+      $secret = hash("md5", $id);
+      return "/dbdebug?id={$id}&secret={$secret}";
+    }
+
+    public function validateDebugParams($id, $secret){
+      return hash("md5", $id) == $secret;
+    }
+    public function install(){
+      // Create Posts Table
+      $promts[] = "CREATE TABLE IF NOT EXISTS posts (
+          pid int(11) NOT NULL AUTO_INCREMENT,
+          title text DEFAULT NULL,
+          dtStart date DEFAULT NULL,
+          dtEnd date DEFAULT NULL,
+          createdBy int(11) NOT NULL,
+          created datetime NOT NULL DEFAULT current_timestamp(),
+          editedBy int(11) DEFAULT NULL,
+          edited datetime DEFAULT NULL,
+          events longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+          PRIMARY KEY (pid)
+        )
+      ";
+      
+      $sql = $this->mysqli->prepare(str_replace("\r\n","",$promt));
+      $sql->execute();
+
+    }
 }
